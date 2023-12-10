266dc29807e73ae3db2cb6a90722f844c3ce499d
you22ef <102329215+you22ef@users.noreply.github.com>
2023-12-11 01:09:24 +0200

    Debuggin and Testing SRFT function

--- Main.java
+++ Main.java
@@ -5,24 +5,28 @@ class Process {
     String color;
     int arrivalTime;
     int burstTime;
+    int original_burstTime;
     int priority;
     int startTime;
     int finishTime;
     int waitingTime;
     int turnaroundTime;
     boolean executed;
+    boolean calculated;
 
     public Process(String name, String color, int arrivalTime, int burstTime, int priority) {
         this.name = name;
         this.color = color;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
+        this.original_burstTime = burstTime;
         this.priority = priority;
-        this.startTime = 0;
+        this.startTime = -1;
         this.finishTime = 0;
         this.waitingTime = 0;
         this.turnaroundTime = 0;
         this.executed = false;
+        this.calculated = false;
     }
 }
 
@@ -62,7 +66,7 @@ public class Main {
         }
 
         //sjfScheduler(new ArrayList<>(processes), contextSwitchingTime); DONE
-        // srtfScheduler(new ArrayList<>(processes), contextSwitchingTime);
+        //srtfScheduler(new ArrayList<>(processes), contextSwitchingTime);DONE
         // priorityScheduler(new ArrayList<>(processes), contextSwitchingTime);
 
         scanner.close();
@@ -98,50 +102,88 @@ public class Main {
                 currentTime++;
             }
         }
+
+        System.out.println("\n" + "Shortest-Job First" + " Execution Order:");
+        for (Process p : sjfOrder) {
+            System.out.println("Process " + p.name + " (" + p.color + ")" + ":\t" + p.startTime + " ---- " + p.finishTime );
+        }
+
         calculateAverages(sjfOrder, "Shortest-Job First ");
     }
 
+
+
+
+
+
     public static void srtfScheduler(List<Process> processes, int contextSwitchingTime) {
-        List<Process> srtfOrder = new ArrayList<>();
         int currentTime = 0;
-
+        int remainingTime = Integer.MAX_VALUE;
+        int idx = -1;
+        Process currentProcess = null;
+        List<Process> srtfOrder = new ArrayList<>(); 
+        List<String> executionOrder = new ArrayList<>();
+        processes.sort(Comparator.comparingInt(p -> p.burstTime));
+        boolean flag = false;
         while (true) {
-            Process shortest = null;
-            int shortestTime = Integer.MAX_VALUE; //don't know why
-
-            for (Process p : processes) {
-                if (p.arrivalTime <= currentTime && !p.executed && p.burstTime < shortestTime) {
-                    shortest = p;
-                    shortestTime = p.burstTime;
+            for (int i = 0; i < processes.size(); i++) {
+                Process p = processes.get(i);
+                if (p.arrivalTime <= currentTime && !p.executed && p.burstTime < remainingTime) {
+                     if (currentProcess != null && currentProcess != p) {
+                         executionOrder.add("Process " + currentProcess.name + " interrupted at time " + currentTime);
+                     }
+                    flag = true;
+                    idx = i;
+                    remainingTime = p.burstTime;
+                    currentProcess = p;
+                    executionOrder.add("Process " + p.name + " started at time " + currentTime);
                 }
             }
+            if(flag){
+                Process pro = processes.get(idx);
+                srtfOrder.add(pro);
+                if (pro.startTime == -1) {
+                    pro.startTime = currentTime;
+                }
+                flag = false;
+            }
+            
 
-            if (shortest == null) {
-                boolean flag = true;
+            if (currentProcess == null) {
+                boolean allExecuted = true;
                 for (Process p : processes) {
                     if (!p.executed) {
                         currentTime++;
-                        flag = false;
+                        allExecuted = false;
                         break;
                     }
                 }
-                if (flag) {
+                if (allExecuted) {
                     break;
                 }
             } else {
-                shortest.burstTime--;
+                currentProcess.burstTime--;
+                remainingTime--;
                 currentTime++;
-                if (shortest.burstTime == 0) {
-                    shortest.finishTime = currentTime;
-                    shortest.turnaroundTime = shortest.finishTime - shortest.arrivalTime;
-                    shortest.waitingTime = shortest.turnaroundTime - shortest.burstTime;
-                    shortest.executed = true;
-                    srtfOrder.add(shortest);
+                if (currentProcess.burstTime == 0) {
+                    currentProcess.executed = true;
+                    currentProcess.finishTime = currentTime;
+                    currentProcess.turnaroundTime = currentProcess.finishTime - currentProcess.arrivalTime;
+                    currentProcess.waitingTime = currentProcess.turnaroundTime - currentProcess.original_burstTime;
+                       executionOrder.add("Process " + currentProcess.name + " ended at time " + currentTime);
+                    currentProcess = null;
+                    remainingTime = Integer.MAX_VALUE;
                 }
             }
         }
 
+        System.out.println("Execution order:");
+        for (String event : executionOrder) {
+            System.out.println(event);
+        }
+    
         calculateAverages(srtfOrder, "Shortest Remaining Time First ");
+       
     }
 
     // (3)
@@ -179,16 +221,20 @@ public class Main {
     public static void calculateAverages(List<Process> order, String scheduler) {
         int totalWaitingTime = 0;
         int totalTurnaroundTime = 0;
-
-        System.out.println("\n" + scheduler + " Execution Order:");
+        int counter = 0;
+        //System.out.println("\n" + scheduler + " Execution Order:");
         for (Process p : order) {
-            System.out.println("Process " + p.name + " (" + p.color + ")");
-            totalWaitingTime += p.waitingTime;
-            totalTurnaroundTime += p.turnaroundTime;
+            //System.out.println("Process " + p.name + " (" + p.color + ")" + ":\t" + p.startTime + " ---- " + p.finishTime );
+            if(!p.calculated){
+                counter++;
+                p.calculated = true;
+                totalWaitingTime += p.waitingTime;
+                totalTurnaroundTime += p.turnaroundTime;
+            }
         }
 
-        double avgWaitingTime = (double) totalWaitingTime / order.size();
-        double avgTurnaroundTime = (double) totalTurnaroundTime / order.size();
+        double avgWaitingTime = (double) totalWaitingTime / counter;
+        double avgTurnaroundTime = (double) totalTurnaroundTime / counter;
 
         System.out.println("\nAverage Waiting Time: " + avgWaitingTime);
         System.out.println("Average Turnaround Time: " + avgTurnaroundTime);
